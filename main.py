2802d008c4c781b03338aa0057391c12e6aecaa4
Nikola Arbov <49321625+Arboff@users.noreply.github.com>
2021-10-25 16:49:46 +0300

    Updated Exceptions
    Handles exceptions.
    Support for shortened URL

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
 
 
     try:
+        x = userInput.replace("https://www.youtube.com/watch?v=", "", 1000)
+        x = x[0: 11]
+        thumbUrl = "https://img.youtube.com/vi/" + x + "/0.jpg"
+        
         r = urllib.request.urlopen(thumbUrl)
         localPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + '\\' + x + ".jpg"
         with open(localPath, "wb") as f:
@@ -18,11 +22,30 @@ def main():
 
         print(f"Sucessfuly saved as {x}.jpg at Desktop.")
         os.system('pause')
+        
     except:
-        print("Looks like URL is invalid. Please, try again.")
-        time.sleep(2)
-        os.system('pause')
-        main()
+        try:
+            x = userInput.replace("https://youtu.be/", "", 1000)
+            x = x[0: 11]
+            thumbUrl = "https://img.youtube.com/vi/" + x + "/0.jpg"
+            
+            r = urllib.request.urlopen(thumbUrl)
+            localPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + '\\' + x + ".jpg"
+            with open(localPath, "wb") as f:
+                f.write(r.read())
+
+            print(f"Sucessfuly saved as {x}.jpg at Desktop.")
+            os.system('pause')
+        except:
+            print("Looks like URL is invalid. Please, try again.")
+            time.sleep(2)
+            os.system('pause')
+            main()
+        
+        
 
 if __name__ == '__main__':
     main()
+
+
+    
