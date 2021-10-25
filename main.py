a1a620b7d6e01975a7c75a15483a7786db676ef1
Nikola Arbov <49321625+Arboff@users.noreply.github.com>
2021-10-25 16:41:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import urllib.request
+import os
+import time
+def main():
+    userInput = input("Enter Youtube URL: ")
+
+    x = userInput.replace("https://www.youtube.com/watch?v=", "", 1000)
+    x = x[0: 11]
+    thumbUrl = "https://img.youtube.com/vi/" + x + "/0.jpg"
+
+
+
+    try:
+        r = urllib.request.urlopen(thumbUrl)
+        localPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + '\\' + x + ".jpg"
+        with open(localPath, "wb") as f:
+            f.write(r.read())
+
+        print(f"Sucessfuly saved as {x}.jpg at Desktop.")
+        os.system('pause')
+    except:
+        print("Looks like URL is invalid. Please, try again.")
+        time.sleep(2)
+        os.system('pause')
+        main()
+
+if __name__ == '__main__':
+    main()
